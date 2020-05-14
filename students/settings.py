bc5a41ceb81ce35d24bcdaae26910649b7d2ee2a
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-14 16:27:45 +0800

    Update settings.py

--- students/settings.py
+++ students/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'students.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -110,7 +110,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
